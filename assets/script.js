910bb2d03cc926fe6a26270e2499bd16dff1a500
Adel <adelelghouaouta@gmail.com>
2024-03-14 09:42:56 +0100

    dots

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ const slides = [
 	{
 		"image":"slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
+	},	
 	{
 		"image":"slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
@@ -18,11 +18,17 @@ const slides = [
 ]
 
 const arrowLeft = document.getElementById("arrow_left");
-
 const arrowRight = document.getElementById("arrow_right");
+const dotsContainer = document.getElementById("dots")
 
 console.log(arrowLeft)
 console.log(arrowRight)
+console.log(dots)
+
+
+let bannerImgElement = document.getElementById("banner-img")
+
+let indiceGlobal = 0
 
 function listenToSwipeAction(){
 	arrowLeft.addEventListener("click", function(){
@@ -30,8 +36,46 @@ function listenToSwipeAction(){
 	})
 	arrowRight.addEventListener("click", function(){
 	console.log("click right")
-
 	})
+
 }
 
-listenToSwipeAction()
+listenToSwipeAction() 
+	arrowRight.addEventListener("click", () => {
+		bannerImgElement.setAttribute("src", "./assets/images/slideshow/slide2.jpg")
+	})	
+	arrowLeft.addEventListener("click", () => {
+		slides[i]
+		++i
+	})
+	// dots.addEventListener("click", () => {
+	// 	dots[i]
+	// 	++i
+	// })
+
+function createDots(){
+	for (let i = 0; i < slides.length; i++){
+		const dot = document.createElement("span");
+		dot.id = "dot"+i
+		dot.className = "dot";
+		dotsContainer.appendChild(dot);
+		dot.addEventListener("click", () => behaviorDot(i))
+	}
+	
+}
+
+createDots()
+
+function behaviorDot(index){
+	let imageName = slides[index].image
+	bannerImgElement.src="./assets/images/slideshow/"+ imageName
+console.log(slides[index].image)
+}
+
+	
+
+
+
+
+
+
