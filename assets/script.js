0e8ee310637c8e9cd3f51a0e64d8953e6ead3b4e
Adel <adelelghouaouta@gmail.com>
2024-03-22 17:47:47 +0100

    last

--- assets/script.js
+++ assets/script.js
@@ -21,6 +21,7 @@ const arrowLeft = document.getElementById("arrow_left");
 const arrowRight = document.getElementById("arrow_right");
 const dotsContainer = document.getElementById("dots");
 
+
 let bannerImgElement = document.getElementById("banner-img");
 let indiceGlobal = 0;
 
@@ -33,8 +34,7 @@ function createDots(){
 		dotsContainer.appendChild(dot);
 		dot.addEventListener("click", () => behaviorDot(i));
 	}
-
-	const dots = document.querySelectorAll(".dot")
+	const dots = document.querySelectorAll(".dot");
 	dots[0].classList.add('dot_selected')
 }
 
@@ -50,26 +50,21 @@ function updateDots(index) {
 }
 
 function behaviorDot(index){
-	let imageName = slides[index].image;
-	let imageDescription = slides[index].tagLine;
+	const imageName = slides[index].image;
+	const imageDescription = slides[index].tagLine;
 	bannerImgElement.src="./assets/images/slideshow/"+ imageName;
 	document.querySelector('p').innerHTML = imageDescription;
 	updateDots(index);
 }
 
-function prevSlide(){
-	indiceGlobal = (indiceGlobal - 1 +slides.length) % slides.length;
-	let imageName = slides[indiceGlobal].image;
-	let imageDescription = slides[indiceGlobal].tagLine;
-	bannerImgElement.src="./assets/images/slideshow/"+ imageName;
-	document.querySelector('p').innerHTML = imageDescription;
-	updateDots(indiceGlobal);
-}
-
-function nextSlide(){
-	indiceGlobal = (indiceGlobal + 1) % slides.length;
-	let imageName = slides[indiceGlobal].image;
-	let imageDescription = slides[indiceGlobal].tagLine;
+function behaviorActionSlide(isRightArrow){
+	if (isRightArrow){
+		indiceGlobal = (indiceGlobal + 1) % slides.length;
+	} else {
+		indiceGlobal = (indiceGlobal - 1 +slides.length) % slides.length;
+	}
+	const imageName = slides[indiceGlobal].image;
+	const imageDescription = slides[indiceGlobal].tagLine;
 	bannerImgElement.src="./assets/images/slideshow/"+ imageName;
 	document.querySelector('p').innerHTML = imageDescription;
 	updateDots(indiceGlobal);
@@ -77,10 +72,10 @@ function nextSlide(){
 
 function listenToSwipeAction(){
 	arrowLeft.addEventListener("click", function(){
-	prevSlide();
+	behaviorActionSlide(false)
 	})
 	arrowRight.addEventListener("click", function(){
-	nextSlide();
+	behaviorActionSlide(true)
 	})
 }
 
